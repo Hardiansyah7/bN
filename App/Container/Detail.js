e12a7bab7b4d48d54de295194de9c2dcdf941088
Hardiansyah <hardigs3fc@gmail.com>
2020-01-04 16:05:42 +0700

    fix detail

--- App/Container/Detail.js
+++ App/Container/Detail.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { Text, View,StyleSheet,ScrollView, BackHandler} from 'react-native'
+import { Text, View,StyleSheet,ScrollView,BackHandler} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default class Detail extends Component {
-    
+
     componentDidMount() {
         BackHandler.addEventListener('hardwareBackPress', this.onBackEvent)
     }
@@ -23,7 +23,7 @@ export default class Detail extends Component {
 
                 <View style={{backgroundColor: '#ff9800',justifyContent:'center',alignItems:"center",height:150}}>
                     <Text style={styles.textUtama}> {this.props.navigation.state.params.nama} </Text>
-                    <Text style={styles.textKedua}> {this.props.navigation.state.params.alias} </Text>
+                    <Text style={styles.textAlias}> {this.props.navigation.state.params.alias} </Text>
                 </View>
                 <View style={{flexDirection:"row"}}>
                     <View style={{backgroundColor: '#00BFFF',justifyContent:'center',alignItems:"center",height:150,width:'50%'}}>
@@ -35,47 +35,49 @@ export default class Detail extends Component {
                         <Text style={styles.textLabel}>Periode</Text>
                     </View>
                 </View>
-                <View style={styles.iconDetail}>
-                    <View style={{flexDirection:"row"}}>
-                        <Ionicons name='md-pin' size={30} color='orange'/>
-                        <Text style={styles.textJudulDetail}>Tempat Diutus</Text>
+                <View style={{marginBottom:30,display:'flex',justifyContent:"space-around"}}>
+                    <View style={{margin:10,marginLeft:14}}>
+                        <View style={{flexDirection:"row"}}>
+                            <Ionicons name='md-pin' size={30} color='orange'/>
+                            <Text style={styles.textJudulDetail}>Tempat Diutus</Text>
+                        </View>
+                        <Text style={styles.textDetail}>{this.props.navigation.state.params.tempat_diutus}</Text>
                     </View>
-                    <Text style={styles.textDetail}>{this.props.navigation.state.params.tempat_diutus}</Text>
-                </View>
-                <View style={styles.iconDetail}>
-                    <View style={{flexDirection:"row"}}>
-                        <Ionicons name='ios-book' size={30} color='orange'/>
-                        <Text style={styles.textJudulDetail}>Disebut Dalam Al-Quran</Text>
+                    <View style={styles.iconDetail}>
+                        <View style={{flexDirection:"row"}}>
+                            <Ionicons name='ios-book' size={30} color='orange'/>
+                            <Text style={styles.textJudulDetail}>Disebut Dalam Al-Quran</Text>
+                        </View>
+                        <Text style={styles.textDetail}>{this.props.navigation.state.params.disebut}</Text>
                     </View>
-                    <Text style={styles.textDetail}>{this.props.navigation.state.params.disebut}</Text>
-                </View>
-                <View style={styles.iconDetail}>
-                    <View style={{flexDirection:"row"}}>
-                        <Ionicons name='md-git-network' size={30} color='orange'/>
-                        <Text style={styles.textJudulDetail}>Jumlah Keturunan</Text>
+                    <View style={styles.iconDetail}>
+                        <View style={{flexDirection:"row"}}>
+                            <Ionicons name='md-git-network' size={30} color='orange'/>
+                            <Text style={styles.textJudulDetail}>Jumlah Keturunan</Text>
+                        </View>
+                        <Text style={styles.textDetail}>{this.props.navigation.state.params.keturunan}</Text>
                     </View>
-                    <Text style={styles.textDetail}>{this.props.navigation.state.params.keturunan}</Text>
-                </View>
-                <View style={styles.iconDetail}>
-                    <View style={{flexDirection:"row"}}>
-                        <Ionicons name='ios-people' size={30} color='orange'/>
-                        <Text style={styles.textJudulDetail}>Sebutan Kaum</Text>
+                    <View style={styles.iconDetail}>
+                        <View style={{flexDirection:"row"}}>
+                            <Ionicons name='ios-people' size={30} color='orange'/>
+                            <Text style={styles.textJudulDetail}>Sebutan Kaum</Text>
+                        </View>
+                        <Text style={styles.textDetail}>{this.props.navigation.state.params.kaum}</Text>
                     </View>
-                    <Text style={styles.textDetail}>{this.props.navigation.state.params.kaum}</Text>
-                </View>
-                <View style={styles.iconDetail}>
-                    <View style={{flexDirection:"row"}}>
-                        <Ionicons name='ios-remove-circle' size={30} color='orange'/>
-                        <Text style={styles.textJudulDetail}>Tempat Wafat</Text>
+                    <View style={styles.iconDetail}>
+                        <View style={{flexDirection:"row"}}>
+                            <Ionicons name='ios-remove-circle' size={30} color='orange'/>
+                            <Text style={styles.textJudulDetail}>Tempat Wafat</Text>
+                        </View>
+                        <Text style={styles.textDetail}>{this.props.navigation.state.params.tempat_wafat}</Text>
                     </View>
-                    <Text style={styles.textDetail}>{this.props.navigation.state.params.tempat_wafat}</Text>
-                </View>
-                <View style={{marginLeft:15,margin:10}}>
-                    <View style={{flexDirection:"row"}}>
-                        <Ionicons name='ios-bookmark' size={30} color='orange'/>
-                        <Text style={{fontSize:18,marginLeft:16,padding:2}}>Disyar'i kan dari</Text>
+                    <View style={{marginLeft:15,marginTop:10}}>
+                        <View style={{flexDirection:"row"}}>
+                            <Ionicons name='ios-bookmark' size={30} color='orange'/>
+                            <Text style={{fontSize:18,marginLeft:28}}>Disyar'i kan dari</Text>
+                        </View>
+                        <Text style={{marginLeft:45,color:'#545454',}}>Qashash al-Anbiya' Ibn Katsir, Badai' az-Zuhur Imam as-Suyuthi dan selainnya</Text>
                     </View>
-                    <Text style={{marginLeft:35,color:'#545454',}}>Qashash al-Anbiya' Ibn Katsir, Badai' az-Zuhur Imam as-Suyuthi dan selainnya</Text>
                 </View>
             </ScrollView>
         )
@@ -87,7 +89,15 @@ const styles = StyleSheet.create({
     textUtama: {
         color:'white',
         fontSize:40,
-        textAlign:"center"
+        textAlign:"center",
+    },
+    textAlias:{
+        color:'white',
+        fontSize:15,
+        textAlign:"center",
+        fontWeight:"bold",
+        paddingLeft:5,
+        paddingRight:5
     },
     textKedua:{
         color:'white',
@@ -99,18 +109,17 @@ const styles = StyleSheet.create({
     },
     textLabel:{
         color:'white',
-        fontSize:15,
+        fontSize:20,
     },
     iconDetail:{
-        margin:10
+        margin:10,
     },
     textJudulDetail:{
         fontSize:18,
-        marginLeft:10,
-        padding:2
+        marginLeft:20,
     },
     textDetail:{
-        marginLeft:40,
+        marginLeft:46,
         color:'#545454',
     }
 });
